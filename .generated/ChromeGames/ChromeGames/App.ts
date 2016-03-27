bd3fadd121f6642ac5ff69dac39e24b240d35d14
Alex Clementi <Alex Clementi>
2016-03-27 10:58:51 +0200

    maj

---
+++ .generated/ChromeGames/ChromeGames/App.ts
@@ -0,0 +1,14 @@
+"Generated from Java with JSweet 1.0.0 - http://www.jsweet.org"; module ChromeGames.ChromeGames {     
+
+
+
+
+
+/**
+ * Hello world!
+ */
+export class App {         
+
+public static main(args: string[])  
+{             
+console.info("Hello World!");         }     } } ChromeGames.ChromeGames.App.main(null); 
---
+++ js/ChromeGames/ChromeGames/App.js
@@ -0,0 +1,21 @@
+"Generated from Java with JSweet 1.0.0 - http://www.jsweet.org";
+var ChromeGames;
+(function (ChromeGames_1) {
+    var ChromeGames;
+    (function (ChromeGames) {
+        /**
+         * Hello world!
+         */
+        var App = (function () {
+            function App() {
+            }
+            App.main = function (args) {
+                console.info("Hello World!");
+            };
+            return App;
+        }());
+        ChromeGames.App = App;
+    })(ChromeGames = ChromeGames_1.ChromeGames || (ChromeGames_1.ChromeGames = {}));
+})(ChromeGames || (ChromeGames = {}));
+ChromeGames.ChromeGames.App.main(null);
+//# sourceMappingURL=App.js.map
